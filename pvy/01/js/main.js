35f390c820c4c5b8b4651d019b80b4b1c94295fa
vyt2-server <vyt2.11@sspu-opava.cz>
2017-09-19 12:32:34 +0200

    JavaScript - změna obrázku

--- pvy/01/js/main.js
+++ pvy/01/js/main.js
@@ -7,8 +7,38 @@ console.log(document.getElementById('hlavni_nadpis').style);
 
 document.getElementById('hlavni_nadpis')
     .addEventListener('mouseenter', function(){
-        alert('No nazdar');
+        // alert('No nazdar');
         document.getElementById('hlavni_nadpis').innerText = 'Můj nadpis';
         document.getElementById('hlavni_nadpis').style.color = 'red';
     });
 
+document.getElementById('hlavni_nadpis')
+    .addEventListener('mouseleave', function(){
+        // alert('No nazdar');
+        document.getElementById('hlavni_nadpis').innerText = 'První lekce';
+        document.getElementById('hlavni_nadpis').style.color = 'black';
+    });
+    
+document.getElementById("shrek")
+        .addEventListener("mouseenter", function(){
+        this.src= "img/shrek2.png";
+        this.style.border = "5px solid black";
+        this.style.opacity = '0.5';
+        this.style.width = '50%';
+    });
+
+document.getElementById("shrek")
+        .addEventListener("mouseleave", function(){
+        this.src= "img/shrek1.jpg";
+        this.style.border = "none";
+        this.style.opacity = '1';
+        this.style.width = '100%';
+    });
+
+document.getElementById('mocnina')
+        .addEventListener('click', function(){
+        var x = prompt('Zadej číslo');    
+        document.getElementById('vysledek').innerText = Math.pow(x,2);
+        /* Druhá odmocnina */ 
+        //document.getElementById('vysledek').innerText = Math.pow(x,1/2);        
+    });
